3bc4e31472d5266882705ed103a7a9b77524ac79
jennder <der.je@husky.neu.edu>
2020-09-16 21:30:42 -0400

    fix numbers case, add test

--- C/xjson.py
+++ C/xjson.py
@@ -3,7 +3,12 @@ import sys
 import json
 
 def print_result(ordered_output):
+    """
+    Print the JSON object results
+    """
+
     print(json.dumps({'count': len(ordered_output), 'seq': ordered_output}))
+
     reversed_list = ordered_output.copy()
     reversed_list.reverse()
     reversed_list.insert(0, len(ordered_output))
@@ -19,12 +24,19 @@ def valid_json(str):
         return True
     except ValueError:
         return False
-    
+'''
+def is_number(curr_json):
+    length = len(curr_json)
+    if curr_json[length - 1] is in ['E', 'e', '+', '-', '.', '/']:
+        if curr_json[length - 2] != ' ':
+            return True
+    '''
 
 def main():
     ordered_output = []
     maybe_json = ''
     for line in sys.stdin:
+
         if line.rstrip() == '/d':
             break
         else:
@@ -33,11 +45,18 @@ def main():
                 curr_json = maybe_json + char
                 # if maybe_json is valid but curr_json is
                 if valid_json(maybe_json) and not valid_json(curr_json):
-                    ordered_output.append(json.loads(maybe_json))
-                    maybe_json = char
+                    if char in ['E', 'e', '+', '-', '.', '/'] and maybe_json[len(maybe_json) - 1] is not ' ':
+                        maybe_json = curr_json
+                    else:
+                        ordered_output.append(json.loads(maybe_json))
+                        maybe_json = char
                 else:
                     maybe_json = curr_json
-    ordered_output.append(json.loads(maybe_json))
+            if valid_json(maybe_json):
+                ordered_output.append(json.loads(maybe_json))
+                maybe_json = ''
+                
+            # otherwise we should check if maybe_json is valid json and add
             
     print_result(ordered_output)
 
