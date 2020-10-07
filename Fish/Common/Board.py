52194276fef8d305018a550632ed8b412a8c91d1
jennder <der.je@husky.neu.edu>
2020-10-06 22:27:05 -0400

    add docs to tile and board class

--- Fish/Common/Board.py
+++ Fish/Common/Board.py
@@ -1,26 +1,34 @@
-from Tile import *
-from Util import validate_non_neg_int, validate_pos_int
 import math
 import random
 import sys
+from tkinter import *
+
+from Tile import *
+from Util import validate_non_neg_int, validate_pos_int, print_error
 import Constants
+
+master = Tk()
 
 class Board:
 
     def __init__(self, row, col):
         """
         Constructor that initializes the game board of size Row by Col
+
+        :row: int       The number of rows
+        :col: int       The number of columns
         """
-        ## TODO: need to make these private
         self.tiles = [[None for r in range(0, row)] for c in range(0, col)]
         self.col = col
         self.row = row
 
     def init_board(self):
+        """
+        Intializes the game board with Tile objects in each place
+        """
         for c in range(0, self.col):
             for r in range(0, self.row):
                 self.tiles[c][r] = Tile(r, c)
-
 
     def create_board_with_holes(self, holes, min):
         """
@@ -32,8 +40,8 @@ class Board:
         self.init_board()
         validate_non_neg_int(len(holes), min)
         if len(holes) + min > (self.row * self.col):
-            print("usage: Invalid Input: holes + min > Total number of tiles")
-            sys.exit(1)
+            print_error(
+                "usage: Invalid Input: holes + min > Total number of tiles")
         self.set_all(1)
         self.print_board()
         for h in holes:
@@ -43,78 +51,114 @@ class Board:
         self.set_random_tiles(self.col * self.row - (len(holes) + min))
         self.print_board()
 
-
     def create_board_without_holes(self, fish):
+        """
+        Create board with no holes, and set all tiles to have a specific
+        number of fish.
+
+        :fish: int          The number of fish per tile
+        """
         self.init_board()
         validate_pos_int(fish)
         self.set_all(fish)
 
     def get_all_reachable_posn(self, row, col):
-        # get all tiles surrounding given tile
-        # TODO: note that these row and col are for index, 0-indexed
+        """
+        Get all reachable board positions from (row, col)
+
+        :returns: array     List of (row, col) representing all possible reachable positions
+        """
         result = []
-        result.extend(self.get_reachable_posn_dir(row, col, Tile.get_north_coord))
-        result.extend(self.get_reachable_posn_dir(row, col, Tile.get_south_coord))
+        # get reachable positions in every direction
+        result.extend(self.get_reachable_posn_dir(
+            row, col, Tile.get_north_coord))
+        result.extend(self.get_reachable_posn_dir(
+            row, col, Tile.get_south_coord))
         result.extend(self.get_reachable_posn_dir(row, col, Tile.get_nw_coord))
         result.extend(self.get_reachable_posn_dir(row, col, Tile.get_ne_coord))
         result.extend(self.get_reachable_posn_dir(row, col, Tile.get_sw_coord))
         result.extend(self.get_reachable_posn_dir(row, col, Tile.get_se_coord))
         return result
 
-
     def get_reachable_posn_dir(self, row, col, get_dir_coord):
         """
         Get all reachable positions on board from give (row, col) Tile.
         Result is a list of (row, col)
 
-        :returns: array of reachable tiles to the give direction
+        :row: int               Current tile's row
+        :col: int               Current tile's column
+        :get_dir_coord: func    Function to get the next tile in specific direction
+        :returns: array         List of (row, col) reachable tiles to the give direction
         """
         next_tile = get_dir_coord(self.tiles[col][row])
         if self.tile_exists(next_tile):
             result = [next_tile]
-            result.extend(self.get_reachable_posn_dir(next_tile[0], next_tile[1], get_dir_coord))
+            result.extend(self.get_reachable_posn_dir(
+                next_tile[0], next_tile[1], get_dir_coord))
             return result
         else:
             return []
-    
+
     def tile_exists(self, tile_coords):
         """
         Is the tile in bounds and active?
 
-        :returns: bool      True if the tile exists, and is active
+        :tile_coords: tuple     The tile's coordinates to check
+        :returns: bool          True if the tile is within game board dimensions, and is active
         """
         row, col = tile_coords
         return row >= 0 and row < self.row and col >= 0 and col < self.col and self.tiles[col][row].is_active
 
-
     def set_random_tiles(self, num_set):
+        """
+        Randomly assign fish values to given number of tiles on the board.
+
+        :num_set: int       The number of tiles to randomly set
+        """
         rand_tiles = random.sample(range(0, self.col * self.row), num_set)
         for value in rand_tiles:
             r, c = self.get_coordinates_from_num(value)
-            self.tiles[c][r].set_fish(max(1, int(random.random() * Constants.MAX_FISH)))
+            self.tiles[c][r].set_fish(
+                max(1, int(random.random() * Constants.MAX_FISH)))
 
     def get_coordinates_from_num(self, num):
+        """
+        Utility function to get the tile coordinates from a number.
+        Used to randomly pick tiles from the board to set.
+        A tile's number is it's column (zero-indexed) * rows in board + it's row
+
+        :num: int           The tile's number
+        :returns: tuple     The tile's row and col
+        """
         col = math.floor(num / self.row)
         row = num % self.row
         return (row, col)
 
     def set_all(self, value):
+        """
+        Set all fish values for every tile in board equal to given value
+
+        :value: int     The fish value to set
+        """
         for col in range(0, self.col):
             for row in range(0, self.row):
                 self.tiles[col][row].set_fish(value)
 
     def remove_tile(self, row, col):
-        if self.tile_exists((row,col)):
+        """
+        Remove the tile at (row, col)
+
+        :row: int      The row of the tile to remove
+        :col: int      The column of the tile to remove
+        """
+        if self.tile_exists((row, col)):
             self.tiles[col][row].create_hole()
 
-    
-    def print_board(self):
-        for c in range(0, self.col):
-            arr = [None] * self.row
-            for r in range(0, self.row):
-                arr[r] = self.tiles[c][r].fish
-    
     def draw_board(self):
+        """
+        Draw the current game board using tkinter canvas.
+        Pass functionality of drawing each tile to the Tile class.
+        """
         w = (self.col * 4 + 1) * self.tiles[0][0].tile_size
         h = (self.row + 1) * self.tiles[0][0].tile_size
         canvas = Canvas(master, width=w, height=h)
--- Fish/Common/Tile.py
+++ Fish/Common/Tile.py
@@ -1,13 +1,20 @@
 from Util import is_even
-from tkinter import *
 from Constants import GUI_UNIT, MAX_FISH
-
-master = Tk()
 
 class Tile:
 
     def __init__(self, row, col):
-        ## TODO: revisit public/private of class variables
+        """
+        Constructor for Tile class.
+
+        :row: int           The tile's row
+        :col: int           The tile's column
+
+        :fish: int          The number of fish this tile has, initially none
+        :is_active: bool    Is the tile active? (has not been removed from board)
+        :tile_size: int     Pixel size to use in drawing
+        """
+        # TODO: revisit public/private of class variables
         self.fish = None
         self.is_active = True
         self.row = row
@@ -15,43 +22,71 @@ class Tile:
         self.tile_size = GUI_UNIT * MAX_FISH
 
     def create_hole(self):
+        """
+        Creates a whole at this Tile. 
+        Set is_active to False, and get rid of fish.
+        """
         self.is_active = False
         self.fish = 0
 
     def set_fish(self, fish):
+        """
+        Set the number of fish equal to given number.
+        Only set the fish count if the Tile is active
+
+        :fish: int      The number of fish to set to
+        """
         if self.is_active:
-            self.fish = fish      
+            self.fish = fish
 
     def get_north_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile north of this Tile
+        """
         return (self.row - 2, self.col)
-    
+
     def get_south_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile south of this Tile
+        """
         return (self.row + 2, self.col)
 
     def get_nw_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile northwest of this Tile
+        """
         if is_even(self.row):
             return (self.row - 1, self.col - 1)
         else:
             return (self.row - 1, self.col)
-    
+
     def get_ne_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile northeast of this Tile
+        """
         if is_even(self.row):
             return (self.row - 1, self.col)
         else:
             return (self.row - 1, self.col + 1)
-    
+
     def get_sw_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile southwest of this Tile
+        """
         if is_even(self.row):
             return (self.row + 1, self.col - 1)
         else:
             return (self.row + 1, self.col)
 
     def get_se_coord(self):
+        """
+        Get the coordinate (row, col) of the Tile southeast of this Tile
+        """
         if is_even(self.row):
             return (self.row + 1, self.col)
         else:
             return (self.row + 1, self.col + 1)
-            
+
     def get_points(self, offset):
         """
         Gets the coordinates for each point on a hexagon of a certain size
@@ -60,7 +95,8 @@ class Tile:
         :return: array    List of points on hexagon
         """
         x_off, y_off = offset
-        points = [self.tile_size, 0,
+        points = [
+            self.tile_size, 0,
             self.tile_size * 2, 0,
             3 * self.tile_size, self.tile_size,
             2 * self.tile_size, 2 * self.tile_size,
@@ -74,7 +110,7 @@ class Tile:
                 points[p] += y_off
 
         return points
-        
+
     def draw_tile(self, canvas, offset):
         """
         Draw the hexagon on the given canvas using the points
@@ -83,21 +119,33 @@ class Tile:
         :canvas: tkinter.Canvas     Canvas to draw on
         """
         color = 'orange' if self.is_active else 'gray'
-        canvas.create_polygon(self.get_points(offset), outline='white', fill=color, tags="hex")
+        canvas.create_polygon(self.get_points(offset), outline='white', fill=color)
         canvas.pack()
 
     def draw_tile_fish(self, canvas):
+        """
+        Draw the tile on given canvas at offset with all the fish.
+        """
         offset = self.get_offset()
         self.draw_tile(canvas, offset)
         self.draw_fish(canvas, offset)
 
     def draw_fish(self, canvas, offset):
-        x_off, y_off = offset
-
+        """
+        Draw all the fish on the canvas
+        """
         for i in range(0, self.fish):
             self.draw_single_fish(canvas, offset, i)
 
     def draw_single_fish(self, canvas, offset, i):
+        """
+        Draw single fish on the canvas. 
+        A fish image is an Oval with a Triangle tail.
+
+        :canvas: tkinter.Canvas     The canvas to draw on
+        :offset: tuple              The (x, y) offset to draw this Tile
+        :i:      int                The index to draw fish vertically aligned
+        """
         x_off, y_off = offset
 
         x0 = self.tile_size + x_off
@@ -107,12 +155,14 @@ class Tile:
         canvas.create_oval(x0, y0, x1, y1, fill='blue')
 
         points = [x1, y0 + (.5 * GUI_UNIT),
-            x1 + (.25 * GUI_UNIT), y0,
-            x1 + (.25 * GUI_UNIT), y1]
+                  x1 + (.25 * GUI_UNIT), y0,
+                  x1 + (.25 * GUI_UNIT), y1]
         canvas.create_polygon(points, fill='blue')
 
-
     def get_offset(self):
+        """
+        Get the (x, y) offset for this Tile in order to draw on tkinter.Canvas
+        """
         x_off = 4 * self.tile_size * self.col
         if not is_even(self.row):
             x_off += 2 * self.tile_size
