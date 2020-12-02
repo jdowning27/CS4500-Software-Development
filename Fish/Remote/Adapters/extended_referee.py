a80d0db67f7fba1ac3a805883620c133a84f5b9e
jdowning27 <josephdowning27@gmail.com>
2020-12-01 19:39:44 -0500

    added doc string to extended referee

--- Fish/Remote/Adapters/extended_referee.py
+++ Fish/Remote/Adapters/extended_referee.py
@@ -6,6 +6,14 @@ from Admin.referee import Referee
 class ExtendedReferee(Referee):
 
     def _Referee__assign_player_colors(self, players):
+        """
+        assigns the player colors to all players in this game. it also informs
+        players about the other colors that are playing in this game
+        Effect: calls the super().__assign_player_colors method, and then calls 
+            player.play_with() for all players in the game with the colors of their opponents
+
+        [List-of PlayerInterface] -> Void
+        """
         internal_players = super()._Referee__assign_player_colors(players)
         for player in super().get_players():
             colors_in_play = super().get_players_as_colors()
